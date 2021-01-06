ec8ddbd1976c18eb360bba7db4524bc9f155aadf
Kozer2 <benhemann@yahoo.com>
2021-01-06 10:29:57 -0600

    Created scaffolding

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 'use strict'
-console.log()
+console.log('app.js is connected')
+
+//global variable
+var userPoints = 0;
+
+alert('Hello, and welcome to the Guessing Game');
+var user = prompt('What is your name?');
+console.log(user);
